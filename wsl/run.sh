a8d729a1baf869ce67e82ddcd91ea06f00d381bd
Malvin Todorov <malvintodorov@gmail.com>
2017-11-06 04:55:57 +0000

    Fix scipt order

--- wsl/run.sh
+++ wsl/run.sh
@@ -7,13 +7,13 @@ cd ~/software/install_logs/
 # LOG symblinks.sh                                                            #
 ###############################################################################
 sudo touch log_install.sh-$(date "+%Y-%m-%d-%H-%M").txt
-sudo unbuffer ~/.dotfiles/wsl/symblinks.sh 2>&1 | tee -a log_install.sh-$(date "+%Y-%m-%d-%H-%M").txt
+sudo unbuffer ~/.dotfiles/wsl/install.sh 2>&1 | tee -a log_install.sh-$(date "+%Y-%m-%d-%H-%M").txt
 sleep 1s
 ###############################################################################
 # LOG install.sh                                                              #
 ###############################################################################
 sudo touch log_symblinks.sh-$(date "+%Y-%m-%d-%H-%M").txt
-sudo unbuffer ~/.dotfiles/wsl/install.sh 2>&1 | tee -a log_symblinks.sh-$(date "+%Y-%m-%d-%H-%M").txt
+sudo unbuffer ~/.dotfiles/wsl/symblinks.sh 2>&1 | tee -a log_symblinks.sh-$(date "+%Y-%m-%d-%H-%M").txt
 ###############################################################################
 # SOURCE CONFIG                                                               #
 ###############################################################################
