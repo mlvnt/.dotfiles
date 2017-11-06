c266e62ec6b66b948d1711aeba010b70c77e4ce0
Malvin Todorov <malvintodorov@gmail.com>
2017-11-06 04:46:24 +0000

    Add expect

--- wsl/begin.sh
+++ wsl/begin.sh
@@ -4,6 +4,7 @@
 sudo apt-get update
 sudo apt-get upgrade
 dpkg -l | grep -qw dos2unix || sudo apt-get install -yyq dos2unix
+dpkg -l | grep -qw expect || sudo apt-get install -yyq expect
 ###############################################################################
 # GIT                                                                         #
 ###############################################################################
@@ -19,9 +20,9 @@ dpkg -l | grep -qw git || sudo apt-get install -yyq git
 # sudo chmod +x begin.sh
 # git clone /mnt/d/Workspace/General/Tech/Reference/OSs/Unix/dotfiles/.dotfiles
 git clone https://github.com/mlvnt/.dotfiles.git ~/.dotfiles
-cd .dotfiles/wsl/
+cd ~/.dotfiles/wsl/
 sudo dos2unix ./.* ./*.* ./shell/.* ./shell/*.* ./editors/.* ./editors/*.* ./git/.* ./git/*.* ./bin/*
 sudo chmod +x install.sh run.sh symblinks.sh begin.sh
 sleep 10s
-sudo ~/.dotfiles/wsl/run.sh
+sudo ./run.sh
 printf "\n“-----------------------\n\n    Install Complete!   \n\n-----------------------”\n\n"
